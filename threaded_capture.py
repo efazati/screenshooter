606ee7f8c086908fa49e40a6a6b945cbee421d70
efazati <mohammad@efazati.org>
2017-02-20 16:59:14 +0330

    make threaded better

--- threaded_capture.py
+++ threaded_capture.py
@@ -1,10 +1,18 @@
 import threading
 import time
+from Queue import Queue
 from selenium import webdriver
 import StringIO
 import contextlib
 from PIL import Image
 import signal
+import logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG,
+                    format='[%(asctime)s %(threadName)s] %(message)s',
+                    datefmt='%H:%M:%S')
+
+q = Queue()
 
 class Screenshooter(object):
     """ Threading example class
@@ -12,13 +20,14 @@ class Screenshooter(object):
     until the application exits.
     """
 
-    def __init__(self, interval=1):
+    def __init__(self, urls, interval=1):
         """ Constructor
         :type interval: int
         :param interval: Check interval, in seconds
         """
         self.interval = interval
         self.driver_obj = webdriver.PhantomJS
+        self.urls = urls
         self.run()
 
     @contextlib.contextmanager
@@ -36,7 +45,7 @@ class Screenshooter(object):
         driver.implicitly_wait(10)
         driver.get(url)
         path = self.image_name(url)
-        print 'Download %s as %s' % (url, path)
+        logger.info('Download %s as %s' % (url, path))
         driver.set_window_size(1920, 1080) # optional
         driver.save_screenshot(path)
         # screen = driver.get_screenshot_as_png() 
@@ -49,19 +58,28 @@ class Screenshooter(object):
         with self.quitting(self.driver_obj()) as driver:
             self.save_image(driver, url)
             t = time.time() - startTime
-        print "%s: %.3f" % (url, t)
+        logger.info("Finished Time %s: %.3f" % (url, t))
         time.sleep(self.interval)
 
 
+    def worker(self):
+        while True:
+            item = q.get()
+            logger.info("Process %s" % (item))
+            self.runner(item)
+            q.task_done()
+
     def run(self):
         """ Method that runs for range """
-        for item in range(13951202000207, 13951202000218):
-            url = "http://www.farsnews.com/newstext.php?nn=%s" % item
-            print 'Generate', url
-            thread = threading.Thread(target=self.runner, args=(url,))
+        for i in range(8):
+            thread = threading.Thread(target=self.worker, args=())
             thread.daemon = True                            # Daemonize thread
             thread.start()                                  # Start the execution
 
 if __name__ == '__main__':
-    screen = Screenshooter()
+    urls = []
+    for item in range(13951202000207, 13951202000218):
+        q.put('http://www.farsnews.com/newstext.php?nn=%s' % item)
+    screen = Screenshooter(urls)
     signal.pause()
+    q.join()
